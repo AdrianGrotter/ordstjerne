4a41064150cae9089c046915b041641f7cdfc28d
JuanMephisto <adrian@grotter.net>
2022-08-30 00:27:53 +0200

    La til ny funksjonalitet: -Skjermen kan roteres -Tilstand lagres når skjermen roteres
    Opprettet ny activity_main.xml fil i mappen layout-land for å få eget layout i landskapsvisning.
    
    Det er nå mørk bakgrunn med hvit tekst. Toolbar'en har også fått en passende farge. Ryddet i funksjoner slik at de er mer lesbare og raskere. Flyttet tekststrenger over i xml-filer så det ikke er noe hardkodet tekst. Forenklet onclick-funksjonene så det er en funksjon istedet for 7.
    
    La også til nytt ord.

--- app/src/main/java/com/example/s354378_ordstjerne/MainActivity.java
+++ app/src/main/java/com/example/s354378_ordstjerne/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.s354378_ordstjerne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
@@ -11,16 +12,31 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 TextView bokstaver;
 TextView score;
 TextView tilbakemelding;
 TextView hintUt;
 TextView funnedeOrd;
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putCharSequence("bokstaverLagret", bokstaver.getText());
+        savedInstanceState.putCharSequence("tilbakemeldingLagret", tilbakemelding.getText());
+        savedInstanceState.putCharSequence("scoreLagret", score.getText());
+        savedInstanceState.putCharSequence("hintLagret", hintUt.getText());
+        savedInstanceState.putCharSequence("funnedeOrdLagret", funnedeOrd.getText());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,6 +46,13 @@ TextView funnedeOrd;
         hintUt = (TextView)findViewById(R.id.textViewHint);
         funnedeOrd = (TextView)findViewById(R.id.funnedeOrd);
 
+        if(savedInstanceState != null){
+            bokstaver.setText(savedInstanceState.getCharSequence("bokstaverLagret"));
+            score.setText(savedInstanceState.getCharSequence("scoreLagret"));
+            tilbakemelding.setText(savedInstanceState.getCharSequence("tilbakemeldingLagret"));
+            hintUt.setText(savedInstanceState.getCharSequence("hintLagret"));
+            funnedeOrd.setText(savedInstanceState.getCharSequence("funnedeOrdLagret"));
+        }
 
 
         Button bokstav1 = (Button)findViewById(R.id.button1);
@@ -40,140 +63,111 @@ TextView funnedeOrd;
         Button bokstav6 = (Button)findViewById(R.id.button6);
         Button bokstav7 = (Button)findViewById(R.id.button7);
 
+        for (Button button : Arrays.asList(bokstav1, bokstav2, bokstav3, bokstav4, bokstav5, bokstav6, bokstav7)) {
+            button.setOnClickListener(this);
+        }
+
+        bokstav1.setTag("B");
+        bokstav2.setTag("D");
+        bokstav3.setTag("I");
+        bokstav4.setTag("E");
+        bokstav5.setTag("O");
+        bokstav6.setTag("U");
+        bokstav7.setTag("Y");
+
         Button angre = (Button)findViewById(R.id.buttonRegret);
         Button sjekk = (Button)findViewById(R.id.buttonSubmit);
         Button hint = (Button)findViewById(R.id.buttonHint);
 
-        bokstav1.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                bokstaver.append("B");
-                tilbakemelding.setText("");
-            }
-        });
-
-        bokstav2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                bokstaver.append("D");
-                tilbakemelding.setText("");
-            }
-        });
-
-        bokstav3.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                bokstaver.append("I");
-                tilbakemelding.setText("");
-            }
-        });
-
-        bokstav4.setOnClickListener(view -> {
-            String letter = "E";
-            bokstaver.append(Html.fromHtml("<font color=#ff0000>" + letter + "</font>", Html.FROM_HTML_MODE_LEGACY));
-            tilbakemelding.setText("");
-        });
-
-        bokstav5.setOnClickListener(view -> {
-            bokstaver.append("O");
-            tilbakemelding.setText("");
-        });
-
-        bokstav6.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                bokstaver.append("U");
-                tilbakemelding.setText("");
-            }
-        });
-
-        bokstav7.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                bokstaver.append("Y");
-                tilbakemelding.setText("");
-            }
-        });
-
-        angre.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                char[] cur = bokstaver.getText().toString().toCharArray();
-                if(cur.length != 0){
-                    StringBuilder newString = new StringBuilder();
-                    for (int i = 0; i<cur.length -1; i++){
-                        newString.append(cur[i]);
-                    }
-                    bokstaver.setText(newString);
+        angre.setOnClickListener(view -> {
+            char[] cur = bokstaver.getText().toString().toCharArray();
+            if(cur.length != 0){
+                bokstaver.setText("");
+                for (int i = 0; i<cur.length -1; i++){
+                    if(cur[i] == 'E') bokstaver.append(Html.fromHtml("<font color=#ff0000>" + "E" + "</font>", Html.FROM_HTML_MODE_LEGACY));
+                    else bokstaver.append(String.valueOf(cur[i]));
                 }
-
             }
+
         });
 
-        sjekk.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                String word = bokstaver.getText().toString();
-                String[] targets = getResources().getStringArray(R.array.ordliste);
-                boolean riktig = false;
-                boolean sjekkMidt = false;
-                if(word.toCharArray().length < 3){
-                    String tilbake = "Ordet er for kort";
+        sjekk.setOnClickListener(view -> {
+            String word = bokstaver.getText().toString();
+            String[] targets = getResources().getStringArray(R.array.ordliste);
+            boolean riktig = false;
+            boolean sjekkMidt = false;
+
+            //Sjekker lengden på ordet
+            if(word.toCharArray().length < 3){
+                String tilbake = "Ordet er for kort";
+                tilbakemelding.setText(tilbake);
+                return;
+            }
+
+            //Sjekker om midterste bokstaven er brukt
+            for(char c : word.toCharArray()){ //Sjekker om E er med
+                if (c == 'E') {
+                    sjekkMidt = true;
+                    break;
+                }
+            }
+
+            //Gir tilbakemelding om E ikke er med
+            if(!sjekkMidt){
+                String tilbake = "Ordet må inneholde E!";
+                tilbakemelding.setText(tilbake);
+                return;
+            }
+
+            //Sjekker om ordet er riktig
+            for(String s : targets){ //Sjekker om ordet er riktig
+                if (s.equals(word)){
+                    riktig=true;
+                    break;
+                }
+            }
+
+            //Hvis ordet er feil
+            if (!riktig) return;
+
+            // Sjekker om ordet er funnet fra før
+            String funnede = funnedeOrd.getText().toString();
+            for (String x : funnede.split(", ")){ //Sjekker om ordet er funnet fra før
+                if (x.equals(word)){
+                    String tilbake = "Denne er allerede funnet!";
                     tilbakemelding.setText(tilbake);
                     return;
                 }
-                for(char c : word.toCharArray()){ //Sjekker om E er med
-                    if (c == 'E') {
-                        sjekkMidt = true;
-                        break;
-                    }
-                }
-                if(!sjekkMidt){
-                        String tilbake = "Ordet må inneholde E!";
-                        tilbakemelding.setText(tilbake);
-                        return;
-                }
-                for(String s : targets){ //Sjekker om ordet er riktig
-                    if (s.equals(word)){
-                        riktig=true;
-                        String funnede = funnedeOrd.getText().toString();
-                        for (String x : funnede.split(",")){ //Sjekker om ordet er funnet fra før
-                            if (x.equals(s)){
-                                System.out.println(x + " "+ s);
-                                String tilbake = "Denne er allerede funnet";
-                                tilbakemelding.setText(tilbake);
-                                return;
-                            }
-                        }
-                        if(!funnede.equals("")) {funnede +=", "+word;}else{funnede = word;}
-                        funnedeOrd.setText(funnede);
-                        break;
-                    }
-                }
-                if (riktig){
-                    char[] currentScore = score.getText().toString().toCharArray();
-                    int oldScore = Character.getNumericValue(currentScore[0]);
-                    if(oldScore < 4){
-                        String newScore = (oldScore+1)+"/4";
-                        score.setText(newScore);
-                        String tilbake = "Riktig!";
-                        tilbakemelding.setText(tilbake);
-                    }else{
-                        System.out.println("Riktig");
-                    }
-                }else{
-                    String tilbake = "Feil svar!";
-                    tilbakemelding.setText(tilbake);
-                    System.out.println("Feil");
-                }
-                bokstaver.setText("");
             }
+
+            //Legger ordet til i oversikten over ord som er funnet
+            if(!funnede.equals("")) funnedeOrd.append(", "+word);
+            else funnedeOrd.append(word);
+
+
+            int oldScore = Character.getNumericValue(score.getText().toString().toCharArray()[0]);
+            String newScore = (oldScore+1)+"/5";
+            score.setText(newScore);
+            String tilbake = "Riktig!";
+            tilbakemelding.setText(tilbake);
+            bokstaver.setText("");
         });
 
-        hint.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                String[] targets = getResources().getStringArray(R.array.ordliste);
-                int num = (int) (Math.random() * (targets.length-1)); //Finner tilfeldig ord
-                char[] word = targets[num].toCharArray();
-                int num2 = (int) (Math.random() * (word.length - 1)); //Trekker tilfeldig sted i ordet som skal byttes ut med *
-                word[num2] = '*';
-                word[num2+1] = '*';
-                String finalHint = String.valueOf(word);
-                hintUt.setText(finalHint);
-            }
+        hint.setOnClickListener(view -> {
+            String[] targets = getResources().getStringArray(R.array.ordliste);
+            int num = (int) (Math.random() * (targets.length-1)); //Finner tilfeldig ord
+            char[] word = targets[num].toCharArray();
+            int num2 = (int) (Math.random() * (word.length - 1)); //Trekker tilfeldig sted i ordet som skal byttes ut med *
+            word[num2] = '*';
+            word[num2+1] = '*';
+            String finalHint = String.valueOf(word);
+            hintUt.setText(finalHint);
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getTag() == "E") bokstaver.append(Html.fromHtml("<font color=#ff0000>" + "E" + "</font>", Html.FROM_HTML_MODE_LEGACY));
+        else bokstaver.append((CharSequence) v.getTag());
     }
 }
